9ffa03e53e29e583669f200e648497dd133ec9bd
Samy Adel <samy.adel@gembitz.com>
2020-10-06 22:14:07 +0100

    Fixed user contant variable

--- routes/secure-routes.js
+++ routes/secure-routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const uuid = require("uuid");
 const bcrypt = require("bcrypt");
 const UserModel = require("../model/model");
-const user = await UserModel.findById(req.user._id);
 
 router.get("/profile", (req, res, next) => {
   res.json({
@@ -12,25 +11,27 @@ router.get("/profile", (req, res, next) => {
 });
 
 router.get("/images", async (req, res, next) => {
+  const user = await UserModel.findById(req.user._id);
+
   res.json({
     images: user.images,
   });
 });
 
 router.post("/post", async (req, res, next) => {
-  res.statusCode = 201;
-
   return (
     await UserModel.findByIdAndUpdate(req.user._id, {
       $push: {
         images: { url: req.body.url, label: req.body.label, uuid: uuid.v1() },
       },
     }),
-    res.json({ images: user.images })
+    res.status(201).json({ images: user.images })
   );
 });
 
 router.post("/deletepost", async (req, res, next) => {
+  const user = await UserModel.findById(req.user._id);
+
   bcrypt.compare(req.body.password, user.password, async (err, result) => {
     if (err) {
       res.status(500).json({ success: false, error: err });
