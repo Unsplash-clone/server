f7612b20866bac65ff12c608ed810a4c38ed6eeb
Samy Adel <samy.adel@gembitz.com>
2020-10-06 13:21:18 +0100

    password protected delete images

--- routes/secure-routes.js
+++ routes/secure-routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const uuid = require("uuid");
+const bcrypt = require("bcrypt");
 const UserModel = require("../model/model");
 
 router.get("/profile", (req, res, next) => {
@@ -25,12 +26,22 @@ router.post("/post", async (req, res, next) => {
 });
 
 router.delete("/post", async (req, res, next) => {
-  await UserModel.findOneAndUpdate(
-    { _id: req.user._id },
-    { $pull: { images: { uuid: req.body.uuid } } }
-  );
+  const user = await UserModel.findById(req.user._id);
 
-  res.status(202).json({ success: true });
+  bcrypt.compare(req.query.password, user.password, async (err, result) => {
+    if (err) {
+      res.status(500).json({ success: false, error: err });
+    } else if (result) {
+      await UserModel.findOneAndUpdate(
+        { _id: req.user._id },
+        { $pull: { images: { uuid: req.body.uuid } } }
+      );
+
+      res.status(202).json({ success: true });
+    } else {
+      res.status(401).json({ success: false });
+    }
+  });
 });
 
 module.exports = router;
